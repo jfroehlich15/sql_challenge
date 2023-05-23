a6ec1651d03ee8e01fe195130c1550098d843cea
jfroehlich15 <justin@thefroehlichs.com>
2023-05-23 10:06:20 -0400

    added primary keys to all tables

--- EmployeeSQL/Table_Schema.sql
+++ EmployeeSQL/Table_Schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
@@ -8,21 +8,27 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL
+    "dept_no" varchar(4)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" varchar   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
     "birth_date" date   NOT NULL,
     "first_name" varchar   NOT NULL,
     "last_name" varchar   NOT NULL,
-    "sex" varchar   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
@@ -31,12 +37,15 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" varchar   NOT NULL
+    "salary" varchar   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "titles" (
-    "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(30)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
@@ -59,3 +68,4 @@ REFERENCES "titles" ("title_id");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
